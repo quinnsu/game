a2464da3420cc2f4c0ed8cfacceaf3afb3505e15
悄兮 <2509253965@qq.com>
2024-12-28 23:21:13 +0800

    build a navBar

--- gameHub/src/App.jsx
+++ gameHub/src/App.jsx
@@ -1,15 +1,31 @@
-import './App.css'
-import { Button } from '@chakra-ui/react'
+import "./App.css";
+import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 function App() {
   return (
-    
-    <>
-    <Button >
-      Click me?
-    </Button>
-   </>
-  )
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+      templateColumns={{
+        base: '1fr',
+        lg: '250px 1fr'
+      }}
+    >
+      <GridItem area="nav">
+         <NavBar />
+      </GridItem>
+        <Show above="lg">
+        <GridItem area="aside" paddingX={5}>
+          aside
+        </GridItem> 
+        </Show>
+      <GridItem area="main">
+        main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
- 
+export default App;
--- gameHub/src/main.jsx
+++ gameHub/src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
+import { ChakraProvider} from "@chakra-ui/react"
 import './index.css'
 import App from './App.jsx'
  
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider >
       <App />
     </ChakraProvider>
   </StrictMode>,
 )
+  
