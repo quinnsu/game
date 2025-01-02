b4722a36814835f49c5b0a1df34a1e8a8a7fc20a
悄兮 <2509253965@qq.com>
2025-01-02 14:46:57 +0800

    Implement infinite scroll

--- gameHub/src/components/GameGrid.jsx
+++ gameHub/src/components/GameGrid.jsx
@@ -1,10 +1,12 @@
-import { Box, Button, SimpleGrid, Text } from "@chakra-ui/react";
+import {  SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import GameCard from "./GameCard";
 import useGames from "../hooks/useGames";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
+import InfiniteScroll from "react-infinite-scroll-component";
 import React from "react";
 import { gameQueryPropTypes } from "../propTypes";
+
 const GameGrid = ({ gameQuery }) => {
   const {
     data,
@@ -16,9 +18,18 @@ const GameGrid = ({ gameQuery }) => {
   } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
   if (error) return <Text>{error.message}</Text>;
+
+  // reduce: 用于将数组中的所有元素通过一个累加器函数归并为单个值。
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce  
+  const fetchedGamesCount = data?.pages.reduce((total, page) => total + page.results.length, 0) || 0;
   return (
-    <Box padding="10px">
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={6}>
+
+      <InfiniteScroll
+        dataLength={fetchedGamesCount}
+        hasMore={!!hasNextPage}
+        next={() => fetchNextPage()}
+        loader={<Spinner />}>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={6} padding="10px">
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer key={skeleton}>
@@ -35,12 +46,8 @@ const GameGrid = ({ gameQuery }) => {
           </React.Fragment>
         ))}
       </SimpleGrid>
-      {hasNextPage && (
-        <Button onClick={() => fetchNextPage()} marginY={5}>
-          {isFetchingNextPage ? "Loading more..." : "Load more"}
-        </Button>
-      )}
-    </Box>
+      </InfiniteScroll>
+    
   );
 };
 
--- gameHub/src/hooks/useGames.js
+++ gameHub/src/hooks/useGames.js
@@ -1,6 +1,5 @@
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
-import { all } from "axios";
 
 const apiClient = new APIClient("/games");
 const useGames = (gameQuery) =>
@@ -19,7 +18,8 @@ const useGames = (gameQuery) =>
     getNextPageParam: (lastPage, allPages) =>{
       return lastPage.next ? allPages.length + 1 : undefined;
       // RAWG API返回的数据中有next字段，若有下一页则返回页数，否则返回undefined
-    }
+    },
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours
   });
 
 export default useGames;
