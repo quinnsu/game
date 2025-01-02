c03b99e217403de07385511f0764bae94bf4eb52
悄兮 <2509253965@qq.com>
2025-01-02 14:07:01 +0800

    Implement infinite queries

--- gameHub/src/hooks/useGames.js
+++ gameHub/src/hooks/useGames.js
@@ -1,19 +1,25 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
+import { all } from "axios";
 
 const apiClient = new APIClient("/games");
 const useGames = (gameQuery) =>
-  useQuery({
+  useInfiniteQuery({
     queryKey: ["games", gameQuery],
-    queryFn: () =>
+    queryFn: ({ pageParam=1}) =>
       apiClient.getAll({
         params: {
           genres: gameQuery.genre?.id,
           parent_platforms: gameQuery.platform?.id,
           ordering: gameQuery.sortOrder?.value,
           search: gameQuery.searchText,
+          page: pageParam,
         },
       }),
+    getNextPageParam: (lastPage, allPages) =>{
+      return lastPage.next ? allPages.length + 1 : undefined;
+      // RAWG API返回的数据中有next字段，若有下一页则返回页数，否则返回undefined
+    }
   });
 
 export default useGames;
