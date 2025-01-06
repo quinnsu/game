e9fd303611bea6ddec5953eece36ba7c4a39a67c
悄兮 <2509253965@qq.com>
2025-01-06 10:52:10 +0800

    Build game trailer

---
+++ gameHub/src/components/GameTrailer.jsx
@@ -0,0 +1,19 @@
+import useTrailers from "../hooks/useTrailers.JS";
+import propTypes from "prop-types";
+
+const GameTrailer = ({ gameId }) => {
+  const { data, error, isLoading } = useTrailers(gameId);
+  if (isLoading) return null;
+
+  if (error) throw error;
+
+  const first = data?.results[0];
+  return first ? (
+    <video src={first.data[480]} poster={first.preview} controls />
+  ) : null;
+};
+
+GameTrailer.propTypes = {
+  gameId: propTypes.number.isRequired,
+};
+export default GameTrailer;
--- gameHub/src/pages/GameDetailPage.jsx
+++ gameHub/src/pages/GameDetailPage.jsx
@@ -1,14 +1,14 @@
-
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
 import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import ExpendableText from "../components/ExpendableText";
 import DefinitionItem from "../components/DefinitionItem";
 import CriticScore from "../components/CriticScore";
+import GameTrailer from "../components/GameTrailer";
 const GameDetailPage = () => {
-  const {slug} = useParams();
-  const {data, isLoading, error} = useGame(slug);
-  
+  const { slug } = useParams();
+  const { data, isLoading, error } = useGame(slug);
+
   if (isLoading) return <Spinner />;
   if (error) throw error;
   //console.log(data.description_raw);
@@ -16,31 +16,32 @@ const GameDetailPage = () => {
   return (
     <>
       <Heading>{data.name}</Heading>
-      <ExpendableText> 
-        {description}
-      </ExpendableText>
+      <ExpendableText>{description}</ExpendableText>
       <SimpleGrid columns={2}>
-      <DefinitionItem term="Platforms">
-        {data.parent_platforms?.map(({ platform }) => (
-          <Text key={platform.id}>{platform.name}</Text>
-        ))}
-      </DefinitionItem>
-      <DefinitionItem term="Metascore">
-        <CriticScore score={data.metacritic} />
-      </DefinitionItem>
-      <DefinitionItem term="Genres">
-        {data.genres.map((genre) => (
-          <Text key={genre.id}>{genre.name}</Text>
-        ))}
-      </DefinitionItem>
-      <DefinitionItem term="Publishers">
-        {data.publishers?.map((publisher) => (
-          <Text key={publisher.id}>{publisher.name}</Text>
-        ))}
-      </DefinitionItem>
+        <DefinitionItem term="Platforms">
+          {data.parent_platforms?.map(({ platform }) => (
+            <Text key={platform.id}>{platform.name}</Text>
+          ))}
+        </DefinitionItem>
+        <DefinitionItem term="Metascore">
+          <CriticScore score={data.metacritic} />
+        </DefinitionItem>
+        <DefinitionItem term="Genres">
+          {data.genres.map((genre) => (
+            <Text key={genre.id}>{genre.name}</Text>
+          ))}
+        </DefinitionItem>
+        <DefinitionItem term="Publishers">
+          {data.publishers?.map((publisher) => (
+            <Text key={publisher.id}>{publisher.name}</Text>
+          ))}
+        </DefinitionItem>
       </SimpleGrid>
-    </>
-  )
-}
 
-export default GameDetailPage
+        <GameTrailer gameId={data.id} />
+
+    </>
+  );
+};
+
+export default GameDetailPage;
